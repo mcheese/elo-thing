ce6c0e7522c84fcc78892dd8eb3fbe1a9b150ea8
Cheese <12050603+mcheese@users.noreply.github.com>
2024-09-10 18:00:59 +0200

    temp frontpage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script>
-  import { Card } from 'flowbite-svelte';
+  import { Heading, P, Button, Popover } from 'flowbite-svelte';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { base } from '$app/paths';
 </script>
 
-<Card>
-  <p>Here could be some text.</p>
-</Card>
+<div class="mx-auto mt-16 w-screen max-w-screen-xl text-center">
+  <Heading tag="h1" class="mb-4" customSize="text-4xl font-bold  md:text-5xl lg:text-6xl">
+    [<span class="font-extrabold text-green-500" id="n">Elo thing</span>] is in development
+  </Heading>
+  <Popover class="text-md" triggeredBy="#n">
+  name pending
+  </Popover>
+  <P class="mb-6 text-lg dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+    You are probably not supposed to be here. Stuff is still getting build and there isn't much
+    working yet. Maybe check out the testing list, with some luck it could be up.
+  </P>
+  <Button color="green" href="{base}/list?id=BAAAAAAA">
+    Testing List
+    <ArrowRightOutline class="ms-2 h-6 w-6" />
+  </Button>
+</div>
