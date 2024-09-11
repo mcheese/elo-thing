0f6fb567a611051eb7ed4cdef9998528bde1ad68
Cheese <12050603+mcheese@users.noreply.github.com>
2024-09-11 17:41:52 +0200

    use malloc in release

--- backend/src/main.zig
+++ backend/src/main.zig
@@ -4,10 +4,10 @@ const zap = @import("zap");
 const Endpoints = @import("endpoints.zig");
 const Elo = @import("elo.zig");
 
-const is_debug = builtin.mode == .Debug;
+const dbg = builtin.mode == .Debug;
 
 // otherwise there are no info logs in release mode
-pub const std_options = .{ .log_level = if (is_debug) .debug else .info };
+pub const std_options = .{ .log_level = if (dbg) .debug else .info };
 
 pub fn main() !void {
     if (std.os.argv.len < 2) {
@@ -19,22 +19,21 @@ pub fn main() !void {
     // hardcode everything for now
     const cfg = .{
         .port = 1337,
-        .interface = if (is_debug) "127.0.0.1" else "0.0.0.0",
-        .threads = if (is_debug) 4 else 8,
+        .interface = if (dbg) "127.0.0.1" else "0.0.0.0",
+        .threads = if (dbg) 4 else 8,
         .workers = 1,
         .db_file = std.mem.span(std.os.argv[1]),
         .log_connections = true,
-        .ssl = !is_debug,
+        .ssl = !dbg,
         .ssl_cert = "cert.pem",
         .ssl_cert_key = "key.pem",
         .ssl_servername = "api.elothing.top:1337",
     };
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{
-        .thread_safe = true,
-    }){};
-    defer std.debug.assert(!gpa.detectLeaks());
-    const alloc = gpa.allocator();
+    // in debug use GPA and check leaks, in release use malloc
+    var gpa :if (dbg) std.heap.GeneralPurposeAllocator(.{.thread_safe = true}) else void = {};
+    defer if (dbg) std.debug.assert(!gpa.detectLeaks());
+    const alloc = if (dbg) gpa.allocator() else std.heap.c_allocator;
 
     var elo = try Elo.init(alloc, cfg.db_file, 1 + cfg.threads * cfg.workers);
     defer elo.deinit();
