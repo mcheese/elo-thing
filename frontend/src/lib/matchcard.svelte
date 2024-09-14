333893bdb2ed157e5c1c5c1454ecd87e6981e0bc
Cheese <12050603+mcheese@users.noreply.github.com>
2024-09-14 09:30:17 +0200

    matchcard text align fix

--- frontend/src/lib/matchcard.svelte
+++ frontend/src/lib/matchcard.svelte
@@ -23,7 +23,7 @@
   class="flex size-full h-96 w-96 flex-col overflow-hidden rounded-lg border border-gray-300 bg-white text-gray-500 shadow-md hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
   on:click={() => vote(left ? 'l' : 'r')}
 >
-<div class="h-56 w-96 overflow-hidden self-center">
+  <div class="h-56 w-96 self-center overflow-hidden">
     {#if data.img && data.img_pos}
       <img
         id="img"
@@ -37,11 +37,11 @@
         id="img"
         src={data.img || base + '/placeholder.png'}
         alt=""
-        class="size-full object-cover dark:bg-gray-600 bg-gray-200"
+        class="size-full bg-gray-200 object-cover dark:bg-gray-600"
       />
     {/if}
   </div>
-  <div class={'m-8 flex flex-grow flex-col justify-center ' + (left ? 'text-right' : 'text-left')}>
+  <div class="p-8 flex w-full flex-grow flex-col justify-center text-{left ? 'right' : 'left'}">
     <p class="text-xl font-thin">{data.rating}</p>
     <p class="text-2xl font-bold">{data.name}</p>
   </div>
